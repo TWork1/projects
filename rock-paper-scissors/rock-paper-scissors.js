5e52059e527a1de7ba21c9b378e916b940fd6f55
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-02-08 10:13:08 -0600

    Scrapped code and remade game

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,51 +1,45 @@
 // Generate random Rock, Paper, or Scissors for Computer
-function getComputerChoice() {
 
-n = Math.floor(Math.random() * 100)
-    let computerSelection;
-    if (33 >= n){
-        computerSelection = "Rock"
-    }
-    else if (n <= 66){
-        computerSelection = "Scissors"
-    }
-    else {
-        computerSelection = "Paper"
-    }
-    return computerSelection
-}
+
+
+
 function playRound(playerSelection, computerSelection){
-        function getComputerChoice() {
+    function getComputerChoice() {
 
-            n = Math.floor(Math.random() * 100)
-             let computerSelection;
-             if (33 >= n){
-                computerSelection = "Rock"
-             }
-             else if (n <= 66){
-                computerSelection = "Scissors"
-             }
-             else {
-                 computerSelection = "Paper"
-             }
-             return computerSelection
-        }
-    let cap = playerSelection.toUpperCase()
-    let remaining = cap.slice(1)
-    let lowerCase = remaining.toLowerCase(1)
-    let firstLetter = cap.charAt()
-    playerInput = firstLetter + lowerCase
-    return playerInput + getComputerChoice()
-}
-console.log(playRound("rock", ));
-
-/*Ask player for input and force lowercase for all
-let player = prompt("Hello let\'s play rock paper scissors now pick your poision")
-function getPlayerChoice(x){
+        n = Math.floor(Math.random() * 100)
+         let computerSelection;
+         if (33 >= n){
+            computerSelection = "Rock"
+         }
+         else if (n <= 66){
+            computerSelection = "Scissors"
+         }
+         else {
+             computerSelection = "Paper"
+         }
+         return computerSelection
+    }
+    let answer = prompt("Welcome loser! get ready for ROCK, PAPER, SCISSORS! NOW PICK")
+    function playerSelection(x){
     let cap = x.toUpperCase()
     let remaining = cap.slice(1)
     let lowerCase = remaining.toLowerCase(1)
     let firstLetter = cap.charAt()
-    playerInput = firstLetter + lowerCase
-    return playerInput*/
+    return playerInput = firstLetter + lowerCase
+    }
+    "Rock">"Scissors"
+    "scissors"> "Paper"
+    "Paper">"Rock"
+    let winner;
+    if  (playerSelection(answer)> getComputerChoice()){
+        winner = "congrats on your victory loser"
+    } else if (playerSelection(answer)< getComputerChoice()){
+        winner = "Sucks to be a loser doesn\'t it?"
+    } else{
+        winner = "lucky tie loser"
+    }
 
+    return winner
+}
+
+console.log(playRound());
