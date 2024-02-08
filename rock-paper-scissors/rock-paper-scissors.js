b787ffd419e3bdfbdfd9dd4353f0c06be5eb7ed4
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-02-08 12:35:09 -0600

    creat rock paper scissors

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,45 +1,63 @@
 // Generate random Rock, Paper, or Scissors for Computer
+w = 0
+l = 0
+t = 0
+for (let i = 0; i < 5; i++){
+    function playRound(){
+        function getComputerChoice() {
+    
+            n = Math.floor(Math.random() * 100)
+             let computerSelection;
+             if (33 >= n){
+                computerSelection = "Rock"
+             }
+             else if (n <= 66){
+                computerSelection = "Scissors"
+             }
+             else {
+                 computerSelection = "Paper"
+             }
+             return computerSelection
+        }
+        computerSelection = getComputerChoice();
+        
+        let answer = prompt("Welcome loser! get ready for ROCK, PAPER, SCISSORS! NOW PICK")
+        function playerSelection(x){
+        let cap = x.toUpperCase()
+        let remaining = cap.slice(1)
+        let lowerCase = remaining.toLowerCase(1)
+        let firstLetter = cap.charAt()
+        return playerInput = firstLetter + lowerCase
+        }
+        playerInput = playerSelection(answer);
+        let winner;
+        if  (playerInput === "Rock" && computerSelection === "Scissors" ||
+        playerInput === "Paper" && computerSelection === "Rock" ||
+        playerInput === "Scissors" && computerSelection === "Paper"){
+            winner = `congrats on your victory loser, you picked ${playerSelection(answer)} and I picked ${computerSelection}`
+            ++w;
+        } else if (playerInput === "Paper" && computerSelection === "Scissors" ||
+        playerInput === "Scissors" && computerSelection === "Rock" ||
+        playerInput === "Rock" && computerSelection === "Paper"){
+            winner = `Sucks to be a loser doesn\'t it? you picked ${playerSelection(answer)} and I picked ${computerSelection}`
+            ++l;
+        } else{
+            winner = `lucky tie loser, you picked ${playerSelection(answer)} and I picked ${computerSelection}`
+            ++t;
+        }
+        scoreboard = w - l;
+        if (scoreboard > 0){
+            response = "Dang I guess you can't really be called a loser anymore huh?"
+        }else if(scoreboard < 0){
+            response ="Well well well, if it isn\'t the greatest of losers in the flesh HA!"
+        }else{
+            response = "Welp to bad for you I still think you\'re a loser *cough* *cough even if it is just a tie"
+        }
+        
+        return  winner
+        
 
-
-
-
-function playRound(playerSelection, computerSelection){
-    function getComputerChoice() {
-
-        n = Math.floor(Math.random() * 100)
-         let computerSelection;
-         if (33 >= n){
-            computerSelection = "Rock"
-         }
-         else if (n <= 66){
-            computerSelection = "Scissors"
-         }
-         else {
-             computerSelection = "Paper"
-         }
-         return computerSelection
     }
-    let answer = prompt("Welcome loser! get ready for ROCK, PAPER, SCISSORS! NOW PICK")
-    function playerSelection(x){
-    let cap = x.toUpperCase()
-    let remaining = cap.slice(1)
-    let lowerCase = remaining.toLowerCase(1)
-    let firstLetter = cap.charAt()
-    return playerInput = firstLetter + lowerCase
-    }
-    "Rock">"Scissors"
-    "scissors"> "Paper"
-    "Paper">"Rock"
-    let winner;
-    if  (playerSelection(answer)> getComputerChoice()){
-        winner = "congrats on your victory loser"
-    } else if (playerSelection(answer)< getComputerChoice()){
-        winner = "Sucks to be a loser doesn\'t it?"
-    } else{
-        winner = "lucky tie loser"
-    }
-
-    return winner
+    console.log(playRound())
 }
-
-console.log(playRound());
+alert(`five rounds done now whats the outcome? ${w},${l},${t}. ${response}`)
