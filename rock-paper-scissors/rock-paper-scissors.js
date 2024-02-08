806cc49893ddcab74354fdfe4181fadb181a3c96
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-02-08 14:01:07 -0600

    Add comments to code

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -3,10 +3,13 @@ w = 0;
 l = 0;
 t = 0;
 q = 0;
+/*Create a loop that lasts five rounds*/
 for (let i = 0; i < 5; i++){
+    /*Functonal code for actual game in background*/
     function playRound(){
+        /*generate a number value from 1-100 and turn it into text RPS*/
         function getComputerChoice() {
-    
+            
             n = Math.floor(Math.random() * 100)
              let computerSelection;
              if (33 >= n){
@@ -21,16 +24,26 @@ for (let i = 0; i < 5; i++){
              return computerSelection
         }
         computerSelection = getComputerChoice();
+        
         let answer = prompt("Welcome loser! get ready for ROCK, PAPER, SCISSORS! NOW PICK")
+        
+        /*Using prompt x change the first letter to be capitalized*/
         function playerSelection(x){
-        let cap = x.toUpperCase()
-        let remaining = cap.slice(1)
-        let lowerCase = remaining.toLowerCase(1)
-        let firstLetter = cap.charAt()
+            let cap = x.toUpperCase()
+            let remaining = cap.slice(1)
+            let lowerCase = remaining.toLowerCase(1)
+            let firstLetter = cap.charAt()
         return playerInput = firstLetter + lowerCase
         }
+
+         /*Take value from the capitalization function and turn it into a variable*/
         playerInput = playerSelection(answer);
+        
         let winner;
+        
+        /* Makes conditions for winning losing and tieing, gives a "response" and keeps score*/
+        /*extra code for fun if RPS is not entered*/
+        
         if  (playerInput === "Rock" && computerSelection === "Scissors" ||
         playerInput === "Paper" && computerSelection === "Rock" ||
         playerInput === "Scissors" && computerSelection === "Paper"){
@@ -49,6 +62,7 @@ for (let i = 0; i < 5; i++){
         }else{
             winner = alert("STOP THESE DISGUSTING ATTEMPTS TO BREAK MY BELOVID CODE")
         }
+        /*create a way to dictate final outcome through a scoreboard*/
         scoreboard = w - l;
         if (scoreboard > 0){
             response = "Dang I guess you can't really be called a loser anymore huh?"
@@ -63,4 +77,5 @@ for (let i = 0; i < 5; i++){
     }
     console.log(playRound())
 }
+/*generate response using values given in the loop once loop finishes*/
 alert(`five rounds done now whats the outcome? ${w},${l},${t}. ${response}`)
