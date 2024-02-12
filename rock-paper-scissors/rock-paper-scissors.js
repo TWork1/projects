9d923dc1a14c63ea9906fc88fb0d243d6e265d20
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-02-12 16:20:17 -0600

    Some UI added

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -3,79 +3,164 @@ w = 0;
 l = 0;
 t = 0;
 q = 0;
-/*Create a loop that lasts five rounds*/
-for (let i = 0; i < 5; i++){
-    /*Functonal code for actual game in background*/
-    function playRound(){
-        /*generate a number value from 1-100 and turn it into text RPS*/
-        function getComputerChoice() {
-            
-            n = Math.floor(Math.random() * 100)
-             let computerSelection;
-             if (33 >= n){
-                computerSelection = "Rock"
-             }
-             else if (n <= 66){
-                computerSelection = "Scissors"
-             }
-             else {
-                 computerSelection = "Paper"
-             }
-             return computerSelection
-        }
-        computerSelection = getComputerChoice();
-        
-        let answer = prompt("Welcome loser! get ready for ROCK, PAPER, SCISSORS! NOW PICK")
-        
-        /*Using prompt x change the first letter to be capitalized*/
-        function playerSelection(x){
-            let cap = x.toUpperCase()
-            let remaining = cap.slice(1)
-            let lowerCase = remaining.toLowerCase(1)
-            let firstLetter = cap.charAt()
-        return playerInput = firstLetter + lowerCase
-        }
+scoreboard = 0
 
-         /*Take value from the capitalization function and turn it into a variable*/
-        playerInput = playerSelection(answer);
-        
-        let winner;
-        
-        /* Makes conditions for winning losing and tieing, gives a "response" and keeps score*/
-        /*extra code for fun if RPS is not entered*/
-        
-        if  (playerInput === "Rock" && computerSelection === "Scissors" ||
-        playerInput === "Paper" && computerSelection === "Rock" ||
-        playerInput === "Scissors" && computerSelection === "Paper"){
-            winner = `congrats on your victory loser, you picked ${playerSelection(answer)} and I picked ${computerSelection}`
-            ++w;
-        } else if (playerInput === "Paper" && computerSelection === "Scissors" ||
-        playerInput === "Scissors" && computerSelection === "Rock" ||
-        playerInput === "Rock" && computerSelection === "Paper"){
-            winner = `Sucks to be a loser doesn\'t it? you picked ${playerSelection(answer)} and I picked ${computerSelection}`
-            ++l;
-        } else if(playerInput === "Paper" && computerSelection === "Paper" ||
-        playerInput === "Scissors" && computerSelection === "Scissors" ||
-        playerInput === "Rock" && computerSelection === "Rock"){
-            winner = `lucky tie loser, you picked ${playerSelection(answer)} and I picked ${computerSelection}`
-            ++t;
-        }else{
-            winner = alert("STOP THESE DISGUSTING ATTEMPTS TO BREAK MY BELOVID CODE")
-        }
-        /*create a way to dictate final outcome through a scoreboard*/
-        scoreboard = w - l;
-        if (scoreboard > 0){
-            response = "Dang I guess you can't really be called a loser anymore huh?"
-        }else if(scoreboard < 0){
-            response ="Well well well, if it isn\'t the greatest of losers in the flesh HA!"
-        }else if(scoreboard>1){
-            response = "Welp to bad for you I still think you\'re a loser *cough* *cough even if it is just a tie"
-        }
-        return  winner
-        
 
+
+const words = document.querySelector('.words');
+const btn1  = document.querySelector('.btn1');
+btn1.addEventListener('click',rockPlayRound)
+const btn2  = document.querySelector('.btn2');
+btn2.addEventListener('click',paperPlayRound)
+const btn3  = document.querySelector('.btn3');
+btn3.addEventListener('click',scissorsPlayRound)
+
+function rockPlayRound(x) {
+    function getComputerChoice() {
+
+        n = Math.floor(Math.random() * 100)
+         let computerSelection;
+         if (33 >= n){
+            computerSelection = "Rock"
+         }
+         else if (n <= 66){
+            computerSelection = "Scissors"
+         }
+         else {
+             computerSelection = "Paper"
+         }
+         return computerSelection
     }
-    console.log(playRound())
+    computerSelection = getComputerChoice();
+
+    x = "Rock"
+    if  (x === "Rock" && computerSelection === "Scissors" ||
+    x === "Paper" && computerSelection === "Rock" ||
+    x === "Scissors" && computerSelection === "Paper"){
+        winner = `congrats on your victory loser, you picked ${x} and I picked ${computerSelection}`
+        ++w;
+    } else if (x === "Paper" && computerSelection === "Scissors" ||
+    x === "Scissors" && computerSelection === "Rock" ||
+    x === "Rock" && computerSelection === "Paper"){
+        winner = `Sucks to be a loser doesn\'t it? you picked ${x} and I picked ${computerSelection}`
+        ++l;
+    } else if(x === "Paper" && computerSelection === "Paper" ||
+    x === "Scissors" && computerSelection === "Scissors" ||
+    x === "Rock" && computerSelection === "Rock"){
+        winner = `lucky tie loser, you picked ${x} and I picked ${computerSelection}`
+        ++t;
+    }else{
+        winner = "STOP THESE DISGUSTING ATTEMPTS TO BREAK MY BELOVID CODE"
+    }
+   
+    /*create a way to dictate final outcome through a scoreboard*/
+    const content = document.createElement('p')
+    content.classList.add('text')
+    content.textContent =  content.textContent = `${winner} ---- Score (${w},${l},${t})`
+    words.appendChild(content)
+    return 
 }
-/*generate response using values given in the loop once loop finishes*/
-alert(`five rounds done now whats the outcome? ${w},${l},${t}. ${response}`)
+function paperPlayRound(x) {
+    function getComputerChoice() {
+        n = Math.floor(Math.random() * 100)
+         let computerSelection;
+         if (33 >= n){
+            computerSelection = "Rock"
+         }
+         else if (n <= 66){
+            computerSelection = "Scissors"
+         }
+         else {
+             computerSelection = "Paper"
+         }
+         return computerSelection
+    }
+    computerSelection = getComputerChoice();
+
+    x = "Paper"
+    if  (x === "Rock" && computerSelection === "Scissors" ||
+    x === "Paper" && computerSelection === "Rock" ||
+    x === "Scissors" && computerSelection === "Paper"){
+        winner = `congrats on your victory loser, you picked ${x} and I picked ${computerSelection}`
+        ++w;
+    } else if (x === "Paper" && computerSelection === "Scissors" ||
+    x === "Scissors" && computerSelection === "Rock" ||
+    x === "Rock" && computerSelection === "Paper"){
+        winner = `Sucks to be a loser doesn\'t it? you picked ${x} and I picked ${computerSelection}`
+        ++l;
+    } else if(x === "Paper" && computerSelection === "Paper" ||
+    x === "Scissors" && computerSelection === "Scissors" ||
+    x === "Rock" && computerSelection === "Rock"){
+        winner = `lucky tie loser, you picked ${x} and I picked ${computerSelection}`
+        ++t;
+    }else{
+        winner = "STOP THESE DISGUSTING ATTEMPTS TO BREAK MY BELOVID CODE"
+    }
+    /*create a way to dictate final outcome through a scoreboard*/
+    scoreboard = w - l;
+    if (scoreboard > 0){
+        response = "Dang I guess you can't really be called a loser anymore huh?"
+    }else if(scoreboard < 0){
+        response ="Well well well, if it isn\'t the greatest of losers in the flesh HA!"
+    }else if(scoreboard>1){
+        response = "Welp to bad for you I still think you\'re a loser *cough* *cough even if it is just a tie"
+    }
+    const content = document.createElement('p')
+    content.classList.add('text')
+    content.textContent =  content.textContent = `${winner} ---- Score (${w},${l},${t})`
+    words.appendChild(content)
+    return  
+}
+function scissorsPlayRound(x) {
+    function getComputerChoice() {
+            
+        n = Math.floor(Math.random() * 100)
+         let computerSelection;
+         if (33 >= n){
+            computerSelection = "Rock"
+         }
+         else if (n <= 66){
+            computerSelection = "Scissors"
+         }
+         else {
+             computerSelection = "Paper"
+         }
+         return computerSelection
+    }
+    computerSelection = getComputerChoice();
+
+    x = "Scissors"
+    if  (x === "Rock" && computerSelection === "Scissors" ||
+    x === "Paper" && computerSelection === "Rock" ||
+    x === "Scissors" && computerSelection === "Paper"){
+        winner = `congrats on your victory loser, you picked ${x} and I picked ${computerSelection}`
+        ++w;
+    } else if (x === "Paper" && computerSelection === "Scissors" ||
+    x === "Scissors" && computerSelection === "Rock" ||
+    x === "Rock" && computerSelection === "Paper"){
+        winner = `Sucks to be a loser doesn\'t it? you picked ${x} and I picked ${computerSelection}`
+        ++l;
+    } else if(x === "Paper" && computerSelection === "Paper" ||
+    x === "Scissors" && computerSelection === "Scissors" ||
+    x === "Rock" && computerSelection === "Rock"){
+        winner = `lucky tie loser, you picked ${x} and I picked ${computerSelection}`
+        ++t;
+    }else{
+        winner = "STOP THESE DISGUSTING ATTEMPTS TO BREAK MY BELOVID CODE"
+    }
+    /*create a way to dictate final outcome through a scoreboard*/
+    scoreboard = w - l;
+    if (scoreboard > 0){
+        response = "Dang I guess you can't really be called a loser anymore huh?"
+    }else if(scoreboard < 0){
+        response ="Well well well, if it isn\'t the greatest of losers in the flesh HA!"
+    }else if(scoreboard>1){
+        response = "Welp to bad for you I still think you\'re a loser *cough* *cough even if it is just a tie"
+    }
+    const content = document.createElement('p')
+    content.classList.add('text')
+    content.textContent = `${winner} ---- Score (${w},${l},${t})`
+    words.appendChild(content)
+    return  
+    
+}
