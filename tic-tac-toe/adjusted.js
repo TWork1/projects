7a7dd07459c7c20f786ee2223f63213f69be5081
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-03-14 15:28:22 -0600

    add targeted clicking to board

--- tic-tac-toe/adjusted.js
+++ tic-tac-toe/adjusted.js
@@ -1,13 +1,49 @@
-const body = document.querySelector('body');
-const gamescreen = document.createElement('div');
-gamescreen.classList.add('mainboard');
-body.appendChild(gamescreen);
+(function (){
 
-const Gameboard = {gameboard:[1,2,3,4,5,6,7,8,9]};
-const players = {one:'bob',two:'john'};
+ const createplayer = (name, marker)=> {
+    let score = 0
+    const getname = () => name = name;
+    const getmarker = () => marker = marker;
+    const getscore = () => score
+    const givepoint = ()=> score++
+    const resetscore = () =>score = 0
+    const getinfo = () => `${name}, ${marker}`
+    return{givepoint,getscore,resetscore,getname,getmarker,getinfo}}
 
-function createPlyer(player){
-    const playername = player;
-    console.log(playername)
-}
+    //div --> Board interaction code local('global')
+    const tiles = document.querySelectorAll('div .item');
+ 
+    const play = ()=>{
+        const boardupdate = ()=>{
+            const tile = Array.from(tiles);
+            const row1 = tile.slice(0,3);
+            const row2 = tile.slice(3,6);
+            const row3 = tile.slice(6,9);
+            const board = [row1,row2,row3];
+            return board
+        }
+        const boardchanges = ()=>{
+            function turns(){
+                currentplayer = player1;
+                return currentplayer
+            }
+            turns()
+            document.onclick = function (e){
+                touched = e.target.className
+                if(touched === "item" && e.target.innerHTML === ''){
+                    if(currentplayer === player1){
+                        e.target.innerHTML = `${player1.getmarker()}`
+                    }
+                    else{e.target.innerHTML = 'wrong'}
+                }
+            }
+        }
+    return{boardupdate,boardchanges}
+    }
+    player1 = createplayer('John','X');
+    player2 = createplayer ('Dax','O');
+    console.log(player2)
+    runit = play();
+    tile = runit.boardupdate();
 
+ })();
