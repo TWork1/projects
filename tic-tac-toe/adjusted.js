b605bb131384e1b703b78757914bb20171368206
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-03-15 13:30:10 -0600

    create final code for tic-tac-toe board

--- tic-tac-toe/adjusted.js
+++ tic-tac-toe/adjusted.js
@@ -9,9 +9,7 @@
     const resetscore = () =>score = 0
     const getinfo = () => `${name}, ${marker}`
     return{givepoint,getscore,resetscore,getname,getmarker,getinfo}}
-
-    //div --> Board interaction code local('global')
- 
+    const body = document.querySelector('body')
     const play = ()=>{
         const boardupdate = ()=>{
             const tiles = document.querySelectorAll('div .item');
@@ -20,8 +18,6 @@
             const row2 = tile.slice(3,6);
             const row3 = tile.slice(6,9);
             const board = [row1,row2,row3];
-            console.log(board[0][0] === board[0][1])
-            console.log(board[0][0].textContent===board[0][1].textContent)
             return board
 
         }
@@ -33,17 +29,67 @@
             turns()
             document.onclick = function (e){
                 touched = e.target.className
-                if(touched === "item" && e.target.innerHTML === ''){
+                board = runit.boardupdate()
+                 if (touched === "item" && e.target.innerHTML === ''){
                     if(currentplayer === player1){
                         e.target.innerHTML = `${player1.getmarker()}`
+                        e.target.id =`${player1.getname()}`
+                        if(board[0][0].id === board[0][1].id &&
+                            board[0][0].id === board[0][2].id ||
+                            board[1][0].id === board[1][1].id &&
+                            board[1][0].id === board[1][2].id ||
+                            board[2][0].id === board[2][1].id &&
+                            board[2][0].id === board[2][2].id ||
+                            board[0][0].id === board[1][0].id &&
+                            board[0][0].id === board[2][0].id ||
+                            board[0][1].id === board[1][1].id &&
+                            board[0][1].id === board[2][1].id ||
+                            board[0][2].id === board[1][2].id &&
+                            board[0][2].id === board[2][2].id ||
+                            board[0][0].id === board[1][1].id &&
+                            board[0][0].id === board[2][2].id ||
+                            board[0][2].id === board[1][1].id &&
+                            board[0][2].id === board[2][0].id){
+                                runit.newboard();
+                                winner = `winner is ${currentplayer.getname()}`
+                                currentplayer.givepoint()
+                                plays = currentplayer.getscore()
+                                document.querySelector('.player1').textContent = plays
+                                console.log(winner)
+                                return 
+                            }
                         currentplayer = player2;
                     }
                     else{
                         e.target.innerHTML = `${player2.getmarker()}`
+                        e.target.id =`${player2.getname()}`
+                        if(board[0][0].id === board[0][1].id &&
+                            board[0][0].id === board[0][2].id ||
+                            board[1][0].id === board[1][1].id &&
+                            board[1][0].id === board[1][2].id ||
+                            board[2][0].id === board[2][1].id &&
+                            board[2][0].id === board[2][2].id ||
+                            board[0][0].id === board[1][0].id &&
+                            board[0][0].id === board[2][0].id ||
+                            board[0][1].id === board[1][1].id &&
+                            board[0][1].id === board[2][1].id ||
+                            board[0][2].id === board[1][2].id &&
+                            board[0][2].id === board[2][2].id ||
+                            board[0][0].id === board[1][1].id &&
+                            board[0][0].id === board[2][2].id ||
+                            board[0][2].id === board[1][1].id &&
+                            board[0][2].id === board[2][0].id){
+                                runit.newboard();
+                                winner = `winner is ${currentplayer.getname()}`
+                                currentplayer.givepoint()
+                                plays = currentplayer.getscore()
+                                document.querySelector('.player2').textContent = plays
+                                console.log(winner)
+                                return}
                         currentplayer = player1}
+                    
                 }
-                board = runit.boardupdate()
-                console.log(board)
+                
             }
         }
         const newboard =()=>{
@@ -57,16 +103,27 @@
                 newtile.id = `${i+1}`
                 mainboard.appendChild(newtile)
             }
+            
         }
-        const checkwinner =() =>{
-            board = runit.boardupdate()
-
-        }
-    return{boardupdate,boardchanges,newboard,checkwinner}
+    return{boardupdate,boardchanges,newboard,}
     }
     player1 = createplayer('John','X');
     player2 = createplayer ('Dax','O');
     console.log(player2)
     runit = play();
-    tile = runit.boardupdate();
+    runit.boardchanges()
  })();
+ const reset = document.querySelector('BUTTON')
+        reset.addEventListener('click', newboard =()=>{
+            const mainboard = document.querySelector('.mainboard');
+            while (document.querySelector('.mainboard').firstChild){
+                document.querySelector('.mainboard').firstChild.remove()
+            }
+            for(i=0; i<9 ;i++){
+                const newtile = document.createElement('div');
+                newtile.classList.add('item')
+                newtile.id = `${i+1}`
+                mainboard.appendChild(newtile)
+            }
+            
+        })
