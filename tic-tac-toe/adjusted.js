54f5cb2f82fb0de75cdfc1898004d1092b9b2911
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-03-15 11:10:42 -0600

    make board update based on each individual tic created

--- tic-tac-toe/adjusted.js
+++ tic-tac-toe/adjusted.js
@@ -11,21 +11,23 @@
     return{givepoint,getscore,resetscore,getname,getmarker,getinfo}}
 
     //div --> Board interaction code local('global')
-    const tiles = document.querySelectorAll('div .item');
  
     const play = ()=>{
         const boardupdate = ()=>{
+            const tiles = document.querySelectorAll('div .item');
             const tile = Array.from(tiles);
             const row1 = tile.slice(0,3);
             const row2 = tile.slice(3,6);
             const row3 = tile.slice(6,9);
             const board = [row1,row2,row3];
+            console.log(board[0][0] === board[0][1])
+            console.log(board[0][0].textContent===board[0][1].textContent)
             return board
+
         }
         const boardchanges = ()=>{
             function turns(){
                 currentplayer = player1;
-
                 return currentplayer
             }
             turns()
@@ -40,9 +42,27 @@
                         e.target.innerHTML = `${player2.getmarker()}`
                         currentplayer = player1}
                 }
+                board = runit.boardupdate()
+                console.log(board)
             }
         }
-    return{boardupdate,boardchanges}
+        const newboard =()=>{
+            const mainboard = document.querySelector('.mainboard');
+            while (document.querySelector('.mainboard').firstChild){
+                document.querySelector('.mainboard').firstChild.remove()
+            }
+            for(i=0; i<9 ;i++){
+                const newtile = document.createElement('div');
+                newtile.classList.add('item')
+                newtile.id = `${i+1}`
+                mainboard.appendChild(newtile)
+            }
+        }
+        const checkwinner =() =>{
+            board = runit.boardupdate()
+
+        }
+    return{boardupdate,boardchanges,newboard,checkwinner}
     }
     player1 = createplayer('John','X');
     player2 = createplayer ('Dax','O');
