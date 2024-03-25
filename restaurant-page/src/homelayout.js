fe3a81e7cebbdfd3f05ec504a3903f86bc930214
Arnauld <157052334+TWork1@users.noreply.github.com>
2024-03-25 14:58:42 -0600

    change font for call text and adjust size of call text

--- restaurant-page/src/homelayout.js
+++ restaurant-page/src/homelayout.js
@@ -13,6 +13,11 @@ call.classList.add('call')
 call.innerHTML = 'The Coziest and Comfiest Cuisine'
 content.appendChild(call);
 
+const text = document.createElement('div')
+text.classList.add('text');
+text.innerHTML =  'They say an apple a day keeps the doctor away, lets hope that holds true with our <p>Apple Cider Wine <p>'
+content.appendChild(text)
+
 const reserve = document.createElement('Button');
 reserve.classList.add('reserve')
 reserve.innerHTML = "Reservations"
